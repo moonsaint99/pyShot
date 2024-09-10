f755917d0f6b354f02dae0628d77c172de7cd74b
rish <34488393+moonsaint99@users.noreply.github.com>
2024-09-10 11:05:37 -0700

    cryo start
    beginning implementing cryo analysis tools

--- setup.py
+++ setup.py
@@ -7,13 +7,17 @@ setup(
     install_requires=[
         "matplotlib",
         "obspy",
+        "scipy",
+        "numpy",
+        "pandas",
+        "pyqt5"
         # Add other dependencies here
     ],
     author="Rishi Chandra",
     author_email="chandra@arizona.edu",
     description="A tool for picking seismic events in a seismic signal",
-    #long_description=open("README.md").read(),
-    #long_description_content_type="text/markdown",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     url="https://github.com/moonsaint99/pyShot",
     classifiers=[
         "Programming Language :: Python :: 3",
